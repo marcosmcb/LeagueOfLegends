d36222141e56fe19a88415a16e0e3298cedb3a37
gorfosp <ya.rossi_nick@hotmail.com>
2015-09-25 01:07:05 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+# LeagueOfLegends
+/*******************************************
+Trabalho 1 - Estrutura de Dados 2
+            INDEXAÇÃO
+Prof. Tiago A. Almeida
+Autores:
+        Marcos Cavalvante
+        Yanick Oliveira Rossi 408646
+********************************************/
